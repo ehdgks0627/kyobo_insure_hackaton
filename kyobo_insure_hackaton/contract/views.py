49704a55afecd1b97d68541f00e5e9bda327f609
sprout <ehdgks0627@naver.com>
2017-11-26 00:28:17 +0900

    added server communication function

--- kyobo_insure_hackaton/contract/views.py
+++ kyobo_insure_hackaton/contract/views.py
@@ -8,22 +8,37 @@ sock_url = "ss5h.namsu.xyz"
 sock_port = 9947
 
 
-def send_to_server(request):
-    request = json.dumps(request) + "\n"
-    request = request.encode()
+def sock_send(data):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((sock_url, sock_port))
-    sock.send(request)
+    sock.send(data)
     response = sock.recv(4096).decode()
-    print(response)
-    response = json.loads(response)
+
+    return response
+
+
+def send_data(data):
+    request_data = {"function": "register", "json": data}
+    request = json.dumps(request_data) + "\n"
+    request = request.encode()
+    response = json.loads(sock_send(request))
+
+    return response["contract_addr"]
+
+
+def recv_data(contract_addr):
+    request_data = {"function": "search", "contract_addr": contract_addr}
+    request = json.dumps(request_data) + "\n"
+    request = request.encode()
+    response = json.loads(sock_send(request))
+    response = json.loads(sock_send(request))
 
     return response
 
 
 def socket_test(requests):
-    request_data = {"function": "register", "name": "<iframe src='https://naver.com'>", "time": "fuck", "product": "a"}
-    response = send_to_server(request_data)
-    contract_addr = response["contract_addr"]
-    request_data = {"function": "search", "contract_addr": contract_addr}
-    return HttpResponse(str(send_to_server(request_data)))
+    contract_addr = send_data("fuck to jung hwan")
+    print("recv from server - ", contract_addr)
+    data = recv_data("contract_addr")
+    print("recv from server - ", data)
+    return HttpResponse("wow")
