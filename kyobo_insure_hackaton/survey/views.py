8978d0879b70989f85e199ed10504fc5b3c88874
sprout <ehdgks0627@naver.com>
2017-11-25 23:21:30 +0900

    Added model and predict

--- kyobo_insure_hackaton/survey/views.py
+++ kyobo_insure_hackaton/survey/views.py
@@ -1,17 +1,77 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-#from keras.models import Sequential
-#from keras.layers.core import Dense, Activation
-#from keras.optimizers import SGD
-#import numpy as np
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Activation
+from keras.optimizers import SGD
+import numpy as np
+
+# import numpy as np
+
+MODELS = {"1": {}, "2": {}}
+for sex in ["1", "2"]:
+    for age in range(5, 19):
+        MODELS[sex][str(age)] = load_model("models/%s_%s_model.h5" % (sex, age))
+        print("loaded %s %s" % (sex, age))
+
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+
+def scale(value, maxValue, minValue=0):
+    if value >= maxValue:
+        return 1
+    return translate(value, minValue, maxValue, 0, 1)
+
+
+def make_item(item):
+    item[0] = scale(item[0], 200)
+    item[1] = scale(item[1], 120)
+    item[2] = scale(item[2], 100)
+    item[3] = scale(item[3], 2.5)
+    item[4] = scale(item[4], 2.5)
+    item[5] = scale(item[5], 2)
+    item[6] = scale(item[6], 2)
+    item[7] = scale(item[7], 140, 60)
+    item[8] = scale(item[8], 100, 40)
+    item[9] = scale(item[9], 16, 10)
+    item[10] = scale(item[10], 3, 1)
+    return item
 
 
 @csrf_exempt
 def check_answer(requests):
-    data = requests.POST.get("data")
+    sex = requests.POST.get("sex")
+    age = requests.POST.get("age")
+    height = float(requests.POST.get("height"))
+    weight = float(requests.POST.get("weight"))
+    waist = float(requests.POST.get("waist"))
+    sight_left = float(requests.POST.get("sight_left"))
+    sight_right = float(requests.POST.get("sight_right"))
+    hear_left = float(requests.POST.get("hear_left"))
+    hear_right = float(requests.POST.get("hear_right"))
+    bp_high = float(requests.POST.get("bp_high"))
+    bp_lwst = float(requests.POST.get("bp_lwst"))
+    hmg = float(requests.POST.get("hmg", "16.00"))
+    smk_stat_type_cd = float(requests.POST.get("smk_stat_type_cd"))
+    drk_yn = float(requests.POST.get("drk_yn"))
+    quality = MODELS[sex][age].predict(np.asarray([
+        make_item([height, weight, waist, sight_left, sight_right, hear_left, hear_right, bp_high, bp_lwst, hmg,
+         smk_stat_type_cd, drk_yn])]))[0]
+    # TODO connect to model
+    return HttpResponse(str(quality))
 
-    return HttpResponse("hi")
+
+# def TODO API 어떻게 쓸까요오오오오ㅗ
 
 def XORExample(request):
     val1 = float(request.GET.get("val1", "0"))
